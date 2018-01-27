d276b900270ec04942deadd74817bd82aac8c581
Thomas Curnow <curnow@ualberta.ca>
2018-01-27 03:21:17 -0700

    Got a base up for the Grid

---
+++ Transmit Your Heart/Assets/Sprites/GridScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridScript : MonoBehaviour
+{
+    public GameObject playerObject;
+    public Vector3Int playerStartPosition;
+    PlayerScript playerScript;
+    Grid grid;
+    Tilemap tilemap;
+
+    void Start()
+    {
+        grid = GetComponent<Grid>();
+        tilemap = grid.GetComponentInChildren<Tilemap>();
+        playerScript = playerObject.GetComponent<PlayerScript>();
+        playerScript.setGridScript(this);
+        playerScript.setPosition(playerStartPosition);
+    }
+
+    //gives the position of the center of the tile
+    public Vector3 cellToWorldPostition (Vector3Int _position)
+    {
+        return grid.CellToWorld(_position) + grid.cellSize/2.0f;
+    } 
+}
---
+++ Transmit Your Heart/Assets/Sprites/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    Vector3Int position;
+    GridScript gridScript;
+
+    public void setGridScript(GridScript _gridScript)
+    {
+        gridScript = _gridScript;
+    }
+
+    public void setPosition(Vector3Int _position)
+    {
+        position = _position;
+        gameObject.transform.position = gridScript.cellToWorldPostition(_position);
+    }
+}
