e37719e585254c06d1ef1000b1768cab60e6ac61
kobitoko <kobitoko@gmail.com>
2018-01-27 13:22:42 -0700

    player movement quickly

--- Transmit Your Heart/Assets/Sprites/PlayerScript.cs
+++ Transmit Your Heart/Assets/Sprites/PlayerScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
+    public float speed = 5;
     Vector3Int position;
     GridScript gridScript;
 
@@ -15,5 +16,14 @@ public class PlayerScript : MonoBehaviour {
     {
         position = _position;
         gameObject.transform.position = gridScript.cellToWorldPostition(_position);
+    }
+    public void Update()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector3 newPosition = this.gameObject.GetComponent<Transform>().position;
+        newPosition.x += h * speed * Time.deltaTime;
+        newPosition.y += v * speed * Time.deltaTime;
+        this.gameObject.GetComponent<Transform>().position = newPosition;
     }
 }
