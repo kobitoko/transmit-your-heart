ae1663b181d3c70d91eba3c90c31d1de8f41c4f3
Thomas Curnow <curnow@ualberta.ca>
2018-01-28 00:11:56 -0700

    ill beat you at this race ryan

---
+++ Transmit Your Heart/Assets/Scripts/UIInventorySlot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventorySlot : MonoBehaviour {
+
+    GameObject electronicPart;
+    bool objectCollected;
+
+	// Use this for initialization
+	void Start () {
+        objectCollected = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void gatherPart()
+    {
+        electronicPart.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        objectCollected = true;
+    }
+
+    public void setPart(GameObject _gameObject)
+    {
+        electronicPart = _gameObject;
+        _gameObject.transform.position = gameObject.transform.position;
+        _gameObject.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+    }
+}
