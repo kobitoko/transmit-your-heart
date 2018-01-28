c0ed2cd61b2f02259fa7523ac620df9f7395d1d6
Thomas Curnow <curnow@ualberta.ca>
2018-01-27 21:18:54 -0700

    Title Screen and ingame popup menu with settings

---
+++ Transmit Your Heart/Assets/Scripts/GameState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameState : MonoBehaviour {
+
+    public string currentLevel;
+    public GameObject musicSliderObject;
+    Slider musicSlider;
+    public GameObject effectSliderObject;
+    Slider effectSlider;
+
+    public float musicVolume;
+    public float effectVolume;
+
+	// Use this for initialization
+	void Start () {
+        musicSlider = musicSliderObject.GetComponent<Slider>();
+        effectSlider = effectSliderObject.GetComponent<Slider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setMusicVolume()
+    {
+        musicVolume = musicSlider.value;
+    }
+
+    public void setEffectVolume()
+    {
+        effectVolume = effectSlider.value;
+    }
+}
---
+++ Transmit Your Heart/Assets/Scripts/UIMenuPopupScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuPopupScript : MonoBehaviour {
+
+    public GameObject UIMenuPopup;
+	// Use this for initialization
+	void Start () {
+        UIMenuPopup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            UIMenuPopup.SetActive(!UIMenuPopup.activeInHierarchy);
+        }
+    }
+
+    public void shutdownGame()
+    {
+        Application.Quit();
+    }
+}
